d247ef0fae4c38a178976d624583e3537159328a
Pablo Torres <pablo.torres@insight-centre.org>
2017-02-18 22:39:07 +0000

    use sleep mode between readings. Define functions for each action

--- src/temperature.ino
+++ src/temperature.ino
@@ -20,40 +20,50 @@ const char* password  = PASSWORD_value;
 const char* host      = THINGSPEAK_HOST_value;
 const char* key       = THINGSPEAK_API_KEY_value;
 
-
-
 // DHT Settings
 #define DHTPIN D6     // what digital pin we're connected to. If you are not using NodeMCU change D6 to real pin
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
+// Initialize the temperature/ humidity sensor
+DHT dht(DHTPIN, DHTTYPE);
 
-const boolean IS_METRIC = true;
+const int UPDATE_INTERVAL_SECONDS = 300;
 
-// Update every 600 seconds = 10 minutes. Min with Thingspeak is ~20 seconds
-const int UPDATE_INTERVAL_SECONDS = 30;
+typedef struct data {
+  float humidity;
+  float temperature;
+  uint16_t voltage;
+} DATA;
 
 /***************************
  * End Settings
  **************************/
- 
-// Initialize the temperature/ humidity sensor
-DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
   delay(10);
-
-  // We start by connecting to a WiFi network
+  
+  connectWifi();
+  postData(readData());
 
   Serial.println();
+  Serial.println("closing connection");
+  ESP.deepSleep(1 * 60 * 1000000, WAKE_RF_DEFAULT); //, WAKE_RF_DISABLED);  //sleep 1 second
+}
+
+void loop() {
+}
+
+void connectWifi() {
+  //We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -63,55 +73,59 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("connecting to ");
+  Serial.println(host);
 }
 
-void loop() {      
-    Serial.print("connecting to ");
-    Serial.println(host);
-    
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect(host, httpPort)) {
-      Serial.println("connection failed");
-      return;
-    }
+DATA readData() {
+  DATA data;
+  dht.begin();
+  while (true) {
+    data.humidity = dht.readHumidity();
+    data.temperature = dht.readTemperature();
+    data.voltage = ESP.getVcc();   
+    //if data were not properly read, wait for 2 seconds and repeat
+    if ( !isnan(data.humidity) && !isnan(data.temperature) ) break;
+    Serial.println("");
+    Serial.println("Reading failed, repeating");
+    delay(2000);
+  }
+  return data;
+}
 
-    // read values from the sensor
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature(!IS_METRIC);
-    
-    // We now create a URI for the request
-    String url = "/update?api_key=";
-    url += key;
-    url += "&field1=";
-    url += String(temperature);
-    url += "&field2=";
-    url += String(humidity);
-    
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-    
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" + 
-                 "Connection: close\r\n\r\n");
-    delay(10);
-    while(!client.available()){
-      delay(100);
-      Serial.print(".");
-    }
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
-    
-    Serial.println();
-    Serial.println("closing connection");
+void postData(DATA data) {
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  // We now create a URI for the request
+  String url = "/update?api_key=";
+  url += key;
+  url += "&field1=";
+  url += String(data.temperature);
+  url += "&field2=";
+  url += String(data.humidity);
+  url += "&field3=";
+  url += String(data.voltage);
 
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
 
-  // Go back to sleep. If your sensor is battery powered you might
-  // want to use deep sleep here
-  delay(1000 * UPDATE_INTERVAL_SECONDS);
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+      "Host: " + host + "\r\n" + 
+      "Connection: close\r\n\r\n");
+  delay(10);
+  while(!client.available()){
+    delay(100);
+    Serial.print(".");
+  }
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
 }
